f23fdfdef2bc0f80303f0423ec04fa3e1cd53945
paulodichone <pdichone@gmail.com>
2019-07-24 19:13:01 -0700

    Initial Commit

---
+++ Starbux-B4-Go/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Starbux-B4-Go
+//
+//  Created by Paulo Dichone on 7/24/19.
+//  Copyright © 2019 Paulo Dichone. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
